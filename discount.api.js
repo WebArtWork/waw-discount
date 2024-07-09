fe999c4fd7b4a0fd769aa585163edf3d6f5ee1db
Narolskyi Denis <asotizianov@gmail.com>
2024-07-09 19:45:43 +0300

    latest ecommerce updates

--- discount.api.js
+++ discount.api.js
@@ -5,46 +5,40 @@ module.exports = async (waw) => {
 		},
 		get: {
 			ensure: waw.role("admin owner"),
-			query: (req) => {
-				return req.user.is.admin
-					? {}
-					: {
-							$or: [
-								{
-									global: true,
-								},
-								{
-									author: req.user._id,
-								},
-							],
-					  };
+			query: () => {
+				return {};
 			},
 		},
 		update: {
 			ensure: waw.role("admin owner"),
 			query: (req) => {
-				return req.user.is.admin
-					? {
-							_id: req.body._id,
-					  }
-					: {
-							_id: req.body._id,
-							author: req.user._id,
-					  };
+				return { _id: req.body._id };
+			},
+		},
+		unique: {
+			name: 'code',
+			ensure: waw.role("admin owner"),
+			query: (req) => {
+				return { _id: req.body._id };
 			},
 		},
 		delete: {
 			ensure: waw.role("admin owner"),
 			query: (req) => {
-				return req.user.is.admin
-					? {
-							_id: req.body._id,
-					  }
-					: {
-							_id: req.body._id,
-							author: req.user._id,
-					  };
+				return { _id: req.body._id };
 			},
 		},
+	});
+
+	const router = waw.router('/api/discount');
+	router.post('/checkup', async (req, res) => {
+		const discountCode = req.body.code;
+		const discount = await waw.Discount.findOne({ code: discountCode });
+
+		if (discount) {
+			return res.status(200).json({ valid: true, discount: discount });
+		} else {
+			return res.status(404).json({ valid: false, message: 'Discount not found' });
+		}
 	});
 };
